20ed861367ac415347c63f5edc5e66539df903eb
Jerry Bruwes <jbruwes@icloud.com>
2025-06-05 19:10:46 +0200

    2025-06-05

---
+++ pages/0272c228-5f58-4d82-8544-0aa775d17651.vue
@@ -0,0 +1,28 @@
+<template>
+        <el-container class="h-full">
+            <el-aside>Aside</el-aside>
+            <el-container>
+                <el-header>Header</el-header>
+                <el-main><RouterView></RouterView></el-main>
+                <el-footer>Footer</el-footer>
+            </el-container>
+        </el-container>
+</template>
+
+<script setup>
+import { getCurrentInstance } from "vue";
+import ElementPlus from "element-plus";
+import Tres from "@tresjs/core";
+
+const { appContext: { app } } = getCurrentInstance();
+app.use(ElementPlus);
+app.use(Tres);
+</script>
+
+<style>
+@import "./node_modules/element-plus/dist/index.css";
+
+html, body, #app {
+    height: 100%;
+}
+</style>
---
+++ pages/207ddc84-21bd-4f2c-be78-4ed2650c1b8e.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="size-full">
+        <TresCanvas v-bind="gl">
+            <!--TresPerspectiveCamera :position="[0, 0, 15]"></TresPerspectiveCamera-->
+            <!--TresDirectionalLight :position="[0, 0, 1]"></TresDirectionalLight-->
+            <Sky :elevation="1" :azimuth="185"></Sky>
+            <Suspense>
+                <Ocean></Ocean>
+            </Suspense>
+            <TresMesh :position-y="1">
+                <TresBoxGeometry :args="[1, 1, 1]"></TresBoxGeometry>
+                <Grid :args="[10.5, 10.5]" cell-color="#82dbc5" :cell-size="0.6" :cell-thickness="0.5"
+                section-color="#fbb03b" :section-size="2" :section-thickness="1.3" :infinite-grid="true" :fade-from="0"
+                :fade-distance="12" :fade-strength="1"></Grid>
+            </TresMesh>
+            <MapControls></MapControls>
+        </TresCanvas>
+    </div>
+</template>
+
+<script setup>
+import { Ocean, MapControls, Sky, Grid } from "@tresjs/cientos";
+import { TresCanvas } from "@tresjs/core";
+import { ACESFilmicToneMapping, SRGBColorSpace, NoToneMapping } from "three";
+
+/*const gl = {
+    clearColor: '#333',
+    outputColorSpace: SRGBColorSpace,
+    toneMapping: ACESFilmicToneMapping,
+    toneMappingExposure: 1,
+}*/
+
+const gl = {
+    clearColor: '#333',
+    alpha: true,
+    outputColorSpace: SRGBColorSpace,
+    toneMapping: NoToneMapping,
+}
+</script>
